26a1f1091e69661d6df43316d3f18fc80ae86c9a
Francisco Chensan <chensanfrancisco@gmail.com>
2022-08-11 07:53:24 +0000

    Add --force flag to install script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-5.3-1.0.0.1/MLNX_OFED
 sudo tar -xzvf MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64.tgz
 
 cd MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64
-sudo ./mlnxofedinstall --auto-add-kernel-support --without-fw-update
+sudo ./mlnxofedinstall --auto-add-kernel-support --without-fw-update --force
 
 cd ~
 sudo /etc/init.d/openibd restart

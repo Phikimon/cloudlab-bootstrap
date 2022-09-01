9c1f2fcb66cf87e0080abac04de97f2a89d33413
Francisco Chensan <chensanfrancisco@gmail.com>
2022-09-01 09:55:00 +0800

    Add DPDK install script

---
+++ dpdk-install.sh
@@ -0,0 +1,15 @@
+cd /opt
+wget https://fast.dpdk.org/rel/dpdk-20.11.1.tar.xz
+tar -xJvf dpdk-20.11.1.tar.xz
+cd dpdk-stable-20.11.1
+export RTE_SDK=/opt/dpdk-stable-20.11.1
+export RTE_TARGET=x86_64-native-linuxapp-gcc
+meson -Dexamples=all build
+ninja -C build
+ninja -C build install
+ldconfig
+cd ..
+wget https://git.dpdk.org/apps/pktgen-dpdk/snapshot/pktgen-dpdk-pktgen-21.02.0.tar.xz
+tar -xJvf pktgen-dpdk-pktgen-21.02.0.tar.xz
+make
+make install

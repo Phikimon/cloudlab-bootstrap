14b8c37ac96034cd607e438caff8d8e9c3192caa
Francisco Chensan <chensanfrancisco@gmail.com>
2022-12-18 14:28:53 +0800

    Add bluefield forwarding

--- rshim-bluefield-setup.sh
+++ rshim-bluefield-setup.sh
@@ -9,3 +9,9 @@ sudo echo "DISPLAY_LEVEL 1" | sudo tee /dev/rshim0/misc
 sudo cp ~/cloudlab-bootstrap/01-netcfg.yaml /etc/netplan
 sudo systemctl restart systemd-networkd
 sudo netplan apply
+
+sudo echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+sudo iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
+sudo iptables -A FORWARD -o eno1 -j ACCEPT
+sudo iptables -A FORWARD -m state --state ESTABLISHED,RELATED -i eno1 -j ACCEPT
+

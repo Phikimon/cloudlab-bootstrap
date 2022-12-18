43ade75bd516265fd4748050b4908fc69cc717c8
Francisco Chensan <chensanfrancisco@gmail.com>
2022-12-18 13:24:04 +0800

    Fix os install script

--- os-install.sh
+++ os-install.sh
@@ -4,4 +4,4 @@
 sudo wget https://content.mellanox.com/BlueField/BFBs/Ubuntu20.04/DOCA_v1.0_BlueField_OS_Ubuntu_20.04-5.3-1.0.0.0-3.6.0.11699-1-aarch64.bfb
 sudo cp ~/cloudlab-bootstrap/bf.cfg ~
 apt-get install pv
-bfb-install --rshim /dev/rshim0 --bfb DOCA_v1.0_BlueField_OS_Ubuntu_20.04–5.3–1.0.0.0–3.6.0.11699–1-aarch64.bfb --config bf.cfg
+bfb-install --rshim /dev/rshim0 --bfb DOCA_v1.0_BlueField_OS_Ubuntu_20.04-5.3-1.0.0.0-3.6.0.11699-1-aarch64.bfb --config bf.cfg

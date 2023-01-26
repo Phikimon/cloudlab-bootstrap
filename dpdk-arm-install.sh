a375d7cabdb55855609e1f48d751826b570f6368
Francisco Chensan <chensanfrancisco@gmail.com>
2023-01-26 19:53:21 +0800

    Add pktgen for arm

--- dpdk-arm-install.sh
+++ dpdk-arm-install.sh
@@ -13,5 +13,19 @@ ninja -C build
 ninja -C build install
 ldconfig
 cd ..
+wget https://git.dpdk.org/apps/pktgen-dpdk/snapshot/pktgen-dpdk-pktgen-21.02.0.tar.xz
+tar -xJvf pktgen-dpdk-pktgen-21.02.0.tar.xz
+
+sudo sed -i '9d' /usr/local/include/rte_spinlock.h
+sudo sed -i '9d' /usr/local/include/rte_atomic_32.h
+
+cd pktgen-dpdk-pktgen-21.02.0
+make
+make install
+
+cd /opt/pktgen-dpdk-pktgen-21.02.0/Builddir
+sudo ninja install
+
+cd ~
 
 echo 1024 | sudo tee /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages

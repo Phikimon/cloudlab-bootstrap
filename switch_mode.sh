5c45243edf5421d74ce0b1f49a0afe742d840e88
Francisco Chensan <chensanfrancisco@gmail.com>
2023-01-27 18:39:35 +0800

    Add messages in switch_mode script

--- switch_mode.sh
+++ switch_mode.sh
@@ -7,11 +7,22 @@ FILENAMES=$(sudo mst status -v | grep BlueField2 | grep -o "/dev/mst/[a-zA-Z0-9_
 if [ $1 == "separated" ]
 then
         MODE=0
-else
+        echo "Changing to Separated Host Mode"
+        echo "==============================="
+elif [ $1 == "embedded" ]
+then
         MODE=1
+        echo "Changing to Embedded Mode"
+        echo "==============================="
+else
+        echo "Usage: switch_mode.sh separated or switch_mode.sh embedded"
+        exit 1
 fi
 
 for PORT in $FILENAMES
 do
         sudo mlxconfig -y -d $PORT s INTERNAL_CPU_MODEL=$MODE
 done
+
+echo
+echo "Done. Please reboot the machine."

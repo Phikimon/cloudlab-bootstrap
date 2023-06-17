ceceb89b7bcca9c723ec2d4bfa65815e126550a5
Philipp <phila775@gmail.com>
2023-06-17 22:43:21 +0700

    Make it verbose what is being executed

--- os-install.sh
+++ os-install.sh
@@ -3,6 +3,8 @@
 # SSH Username: ubuntu
 # Password: Ubuntu1!
 
+set -x
+
 sudo wget https://content.mellanox.com/BlueField/BFBs/Ubuntu20.04/DOCA_v1.0_BlueField_OS_Ubuntu_20.04-5.3-1.0.0.0-3.6.0.11699-1-aarch64.bfb
 sudo cp ~/cloudlab-bootstrap/bf.cfg ~
 apt-get install pv

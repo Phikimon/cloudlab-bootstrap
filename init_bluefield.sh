fa8956649bea5b93d15c62b300e168299f0c62ef
Philipp <phila775@gmail.com>
2023-06-18 08:29:47 +0700

    Add some necessary commands to be entered manually

--- init_bluefield.sh
+++ init_bluefield.sh
@@ -2,7 +2,12 @@
 
 set -x
 
-echo 'nameserver 8.8.8.8' | sudo tee /etc/resolv.conf
+# Commands that could be required but without which you
+# would not be able to clone this repo:
+#
+# sudo ip addr add 192.168.100.2/30 dev tmfifo_net0
+# sudo ip route add default via 192.168.100.1
+# echo 'nameserver 8.8.8.8' | sudo tee /etc/resolv.conf
 cd ~
 git clone https://github.com/Phikimon/dot_files/
 mv ./dot_files/.vimrc .
